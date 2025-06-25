bd9b72a5b15d0b2f51ba959346fc17a3928fe039
Mariano Laurenti <mla@qubikcommerce.com>
2025-06-25 10:44:11 -0300

    Reapply "first commit"
    This reverts commit 0045a93735979c0a973752eaacbce5fe4d659e8d.

---
+++ src/main/java/com/devspace/conexfy/ConexfyApplication.java
@@ -0,0 +1,13 @@
+package com.devspace.conexfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexfyApplication.class, args);
+	}
+
+}
