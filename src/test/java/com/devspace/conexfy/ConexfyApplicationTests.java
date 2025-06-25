a30822cf94a57c007a070e753927bf8b4c20cad6
Mariano Laurenti <mla@qubikcommerce.com>
2025-06-25 10:42:37 -0300

    first commit

---
+++ src/test/java/com/devspace/conexfy/ConexfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devspace.conexfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
